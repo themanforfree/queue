8d3eef83e0ef637eef6ae6973ea996b5572161d9
themanforfree <themanforfree@gmail.com>
2022-06-24 22:17:21 +0800

    Update benches

--- benches/lock_based_queue.rs
+++ benches/lock_based_queue.rs
@@ -4,7 +4,14 @@ use queue::lock_based_queue::Queue;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut q = Queue::new();
-    c.bench_function("lock_based_queue", |b| b.iter(|| q.enqueue(black_box(20))));
+    c.bench_function("lock_based_queue", |b| {
+        b.iter(|| {
+            for i in 0..black_box(100) {
+                q.enqueue(i);
+                assert_eq!(q.dequeue(), Some(i));
+            }
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
---
+++ benches/lock_free_queue.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use queue::lock_free_queue::Queue;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut q = Queue::new();
+    c.bench_function("lock_free_queue", |b| {
+        b.iter(|| {
+            for i in 0..black_box(100) {
+                q.enqueue(i);
+                assert_eq!(q.dequeue(), Some(i));
+            }
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
