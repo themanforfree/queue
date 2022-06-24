a03d783d1518d4e69ca6d19d542db6118bcc6fe7
themanforfree <themanforfree@gmail.com>
2022-06-24 16:58:40 +0800

    lock_free_queue: Update

--- src/lock_free_queue.rs
+++ src/lock_free_queue.rs
@@ -4,14 +4,21 @@ use std::{
 };
 
 struct Node<T> {
-    value: T,
+    value: Option<T>,
     next: AtomicPtr<Node<T>>,
 }
 
 impl<T> Node<T> {
     fn new(value: T) -> Self {
         Node {
-            value,
+            value: Some(value),
+            next: AtomicPtr::from(ptr::null_mut()),
+        }
+    }
+
+    fn empty() -> Self {
+        Node {
+            value: None,
             next: AtomicPtr::from(ptr::null_mut()),
         }
     }
@@ -22,12 +29,9 @@ pub struct Queue<T> {
     tail: AtomicPtr<Node<T>>,
 }
 
-impl<T> Queue<T>
-where
-    T: Default + Copy,
-{
+impl<T> Queue<T> {
     pub fn new() -> Self {
-        let dummy_node = Box::into_raw(Box::new(Node::new(T::default())));
+        let dummy_node = Box::into_raw(Box::new(Node::empty()));
         let head = AtomicPtr::from(dummy_node);
         let tail = AtomicPtr::new(head.load(Ordering::SeqCst));
         Queue { head, tail }
@@ -38,17 +42,18 @@ where
         let mut p = self.tail.load(Ordering::Acquire);
         let old_p = p;
 
-        let next_node = unsafe { &(*p).next };
-        while next_node
-            .compare_exchange(ptr::null_mut(), q, Ordering::Release, Ordering::Relaxed)
-            .is_err()
-        {
-            unsafe {
+        unsafe {
+            while (*p)
+                .next
+                .compare_exchange(ptr::null_mut(), q, Ordering::Release, Ordering::Relaxed)
+                .is_err()
+            {
                 while !(*p).next.load(Ordering::Acquire).is_null() {
                     p = (*p).next.load(Ordering::Acquire);
                 }
             }
         }
+
         let _ = self
             .tail
             .compare_exchange(old_p, q, Ordering::Release, Ordering::Relaxed);
@@ -56,18 +61,18 @@ where
 
     pub fn dequeue(&mut self) -> Option<T> {
         loop {
-            let h = self.head.load(Ordering::Acquire);
-            let h_next = unsafe { (*h).next.load(Ordering::Acquire) };
-            if h_next.is_null() {
+            let p = self.head.load(Ordering::Acquire);
+            let p_next = unsafe { (*p).next.load(Ordering::Acquire) };
+            if p_next.is_null() {
                 return None;
             }
 
             if self
                 .head
-                .compare_exchange(h, h_next, Ordering::Release, Ordering::Relaxed)
+                .compare_exchange(p, p_next, Ordering::Release, Ordering::Relaxed)
                 .is_ok()
             {
-                return Some(unsafe { (*h_next).value });
+                return unsafe { (*p_next).value.take() };
             }
         }
     }
